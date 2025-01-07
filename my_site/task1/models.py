6042b365b5a18490bd2218b07dce21aad1cb27c7
Tatiana-E <tanya.era1989@gmail.com>
2025-01-07 19:12:22 +0500

    Update models.py

--- my_site/task1/models.py
+++ my_site/task1/models.py
@@ -14,6 +14,7 @@ class Game(models.Model):
     title = models.CharField(max_length=200)
     cost = models.DecimalField(decimal_places=2, max_digits=10)
     size = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.TextField(default="Очень хорошая игра")
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
 
